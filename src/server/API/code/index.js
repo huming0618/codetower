4fbae29310a8d088b7108b8c0a75358539771317
huqiming <huqiming@navercorp.com>
2017-03-21 15:31:26 +0800

    use sourcetree in code api
    [NOTE]
    - the source tree should hide the real path for the API usage
    
    [TODO]
    - for sourcetree, every file/folder should have uuid
    - when generate the source tree, add data to the redis
    - add data layer, the api should get the data from the data layer

--- src/server/API/code/index.js
+++ src/server/API/code/index.js
@@ -5,6 +5,7 @@ const router = require('koa-router')(prefix);
 const path = require('path');
 const config = require(path.resolve(__dirname, '../../../../', 'config'));
 
+const sourceTree = require('./source-tree.js');
 //const router = new Router(prefix);
 
 const repoList = config.repo;
@@ -16,7 +17,10 @@ router.get('/', async (ctx) => {
 
 router.get('/:repo', async (ctx) => {
   const name = ctx.params.repo;
-  ctx.body = name;
+  const path = repoList[name];
+
+  ctx.body = await Promise.resolve(sourceTree(path));
+
 });
 
 module.exports = router;
