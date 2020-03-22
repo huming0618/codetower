0d068821ea31095aa84e0840e7533d1615da7ff2
Peter Hu <yesky93@sina.com>
2020-03-22 23:09:07 +0800

    use isomorphic-git

--- src/home/main.js
+++ src/home/main.js
@@ -1,29 +1,44 @@
 import git from 'isomorphic-git'
 import LightningFS from '@isomorphic-git/lightning-fs';
 
+const GitService = require('../service/GitService').default
+
 const http = require('isomorphic-git/http/web')
 
-const fs = new LightningFS('fs')
-const pfs = fs.promises
-console.log(git, http)
+// const fs = new LightningFS('fs')
+// console.log(git, http)
 
 
-const dir = '/test-clone'
-git.clone({
-    fs,
-    http,
-    dir,
-    url: 'https://github.com/isomorphic-git/lightning-fs',
-    corsProxy: 'https://cors.isomorphic-git.org'
-}).then(async x => {
-    const result = await pfs.readdir(dir);
-    result.forEach(async x => {
-        const stat = await pfs.stat(dir + '/' + x)
-        console.log(x, stat)
+const dir = '/test-react'
+
+const gitService = new GitService(dir)
+
+gitService.init().then(async () => {
+    // const files = await gitService.getFileList()
+    // console.log('gitService.files', files)
+
+    gitService.getFileList$().subscribe(x => {
+        console.log('file item', x)
     })
-    const content = await pfs.readFile(dir + '/' + 'README.md', {
-        encoding: 'utf8'
-    })
-    console.log('result', result)
-    //console.log('result', content)
+    //console.log('init done', result)
 })
+
+// git.clone({
+//     fs,
+//     http,
+//     dir,
+//     url: 'https://github.com/isomorphic-git/lightning-fs',
+//     corsProxy: 'https://cors.isomorphic-git.org'
+// }).then(async x => {
+
+//     // const result = await pfs.readdir(dir);
+//     // result.forEach(async x => {
+//     //     const stat = await pfs.stat(dir + '/' + x)
+//     //     console.log(x, stat)
+//     // })
+//     // const content = await pfs.readFile(dir + '/' + 'README.md', {
+//     //     encoding: 'utf8'
+//     // })
+//     // console.log('result', result)
+//     // //console.log('result', content)
+// })
--- src/service/GitService.ts
+++ src/service/GitService.ts
@@ -1,4 +1,4 @@
-
+import { Observable } from 'rxjs';
 
 const git = require('isomorphic-git')
 const http = require('isomorphic-git/http/web')
@@ -10,43 +10,114 @@ const pfs = fs.promises
 type FileItem = {
     name: string,
     path: string,
-    content: () => Promise<string>
+    content: () => Promise<string>,
+    dir: string
 }
 
-class GitService {
+const sortEntry = (a: string, b: string) => {
+    return a[0].toLowerCase() > b[0].toLowerCase() ? 1 : -1
+}
+
+export default class GitService {
     dir: string = ''
     constructor(dirname: string) {
         this.dir = dirname
     }
 
-    init() {
+    init(): Promise<any> {
+        console.log('to init')
         return git.clone({
             fs,
             http,
             dir: this.dir,
-            url: 'https://github.com/isomorphic-git/lightning-fs',
+            url: 'https://github.com/facebook/react',
             corsProxy: 'https://cors.isomorphic-git.org'
         })
     }
 
-    //https://blog.csdn.net/weixin_44704691/article/details/102639587
-    async getFileList(): Promise<Array<any>> {
-        const result: Array<any> = []
-        const dir = this.dir
-        const entryList = await pfs.readdir(dir);
-        for (let entry in entryList) {
-            const stat = await pfs.stat(dir + '/' + entry)
-            console.log(x, stat)
-        }
-        entryList.forEach(async x => {
-            const stat = await pfs.stat(dir + '/' + x)
-            console.log(x, stat)
-        })
-        const content = await pfs.readFile(dir + '/' + 'README.md', {
-            encoding: 'utf8'
-        })
-        console.log('result', result)
+    static joinPath(...items: Array<string>) {
+        return items.join('/')
+    }
 
-        return result
+    //https://blog.csdn.net/weixin_44704691/article/details/102639587
+
+    getFileList$(): Observable<FileItem> {
+        const baseDir = this.dir
+
+        console.log('getFileList$')
+
+
+        return new Observable((observer: any) => {
+            const read = (theDirPath: string) => {
+                pfs.readdir(theDirPath).then((entryList: Array<string>) => {
+                    entryList.sort(sortEntry).forEach(async (item: string) => {
+                        const itemPath = GitService.joinPath(theDirPath, item)
+                        const stat = await pfs.stat(itemPath)
+
+                        if (stat.type == 'dir') {
+                            // console.log('dir', stat, theDirPath, itemPath)
+                            read(itemPath)
+                        }
+                        else {
+                            // console.log('file', stat, itemPath)
+                            observer.next({
+                                dir: theDirPath,
+                                name: item,
+                                path: itemPath,
+                                content: () => {
+                                    return pfs.readFile(itemPath, {
+                                        encoding: 'utf8'
+                                    })
+                                }
+                            })
+                        }
+                    })
+                })
+            }
+
+            read(baseDir)
+        });
+    }
+
+    async getFileList(ignorePaths: Array<string> = []): Promise<Array<any>> {
+
+
+        const result: Array<FileItem> = []
+        const baseDir = this.dir
+        const queue: Array<string> = []
+        queue.push(baseDir)
+
+        return new Promise(async (resolve) => {
+            while (queue.length) {
+                console.log('queue', queue)
+                let dirPath = queue.shift()
+                if (queue.length === 0) {
+                    resolve(result)
+                }
+                const entryList = await pfs.readdir(dirPath);
+                entryList.forEach(async (item: string) => {
+                    const itemPath = GitService.joinPath(dirPath, item)
+                    const stat = await pfs.stat(itemPath)
+
+                    if (stat.type == 'dir') {
+                        console.log('dir', stat, dirPath, itemPath)
+                        queue.push(itemPath)
+                    }
+                    else {
+                        console.log('file', stat, itemPath)
+                        result.push({
+                            dir: dirPath,
+                            name: item,
+                            path: itemPath,
+                            content: () => {
+                                return pfs.readFile(itemPath, {
+                                    encoding: 'utf8'
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
     }
 }
